61de9893af1fffde5f1aa4510f80a05ab2cfdbf6
Mohamed Khaled <flavasava1212@gmail.com>
2021-12-17 02:30:47 +0200

    handel the config services in the main & the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigService } from "./config/config.service";
+import { ConfigModule } from "./config/config.module";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.appDatabaseType,
+          host: configService.databaseHost,
+          port: configService.databasePort,
+          username: configService.databaseUsername,
+          password: configService.databasePassword,
+          database: configService.databaseName,
+          synchronize: configService.databaseSynchronize,
+          entities: configService.entities,
+          seeds: configService.seeds,
+          factories: configService.factories,
+          subscribers: configService.subscribers,
+          extra: {
+            charset: configService.charset,
+          },
+        }
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(morgan('dev'), helmet());
+  app.enableCors();
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Go plus App')
+    .setDescription('Parking application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(configService.appPort);
 }
 bootstrap();
